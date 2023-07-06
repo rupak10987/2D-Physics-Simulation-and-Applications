bc7b82811aa221c1faa7dd4bddbd11470be7aef2
Rupak Saha <rupak10987@gmail.com>
2023-07-06 21:24:20 +0600

    soft_body

--- Verlet_integration_sphere_physics/boobs.cpp
+++ Verlet_integration_sphere_physics/boobs.cpp
@@ -1,3 +1,4 @@
+
 #include<iostream>
 #include<time.h>
 #include<graphics.h>
@@ -22,7 +23,7 @@ public:
     struct VEC2 current_pos;
     struct VEC2 prev_pos;
     struct VEC2 vel_dt;
-    struct VEC2 acceleration= {0,9.8}; //(meter/sec^2)
+    struct VEC2 acceleration= {0,9.8}; ///m/sec^2
     verlet_balls(class verlet_balls* m_balls[],int unique_indx,float r,struct VEC2 pos)
     {
         this->m_id=unique_indx;
@@ -36,8 +37,8 @@ public:
         if(!is_static)
         {
             this->container_pot();
-            for(int i=0; i<5; i++) //substeps
-                resolve_collision(m_balls,num_of_balls);
+            //for(int i=0;;i++)//substeps
+            resolve_collision(m_balls,num_of_balls);
             vel_dt= {current_pos.X_Pos-prev_pos.X_Pos,current_pos.Y_Pos-prev_pos.Y_Pos}; //s=vt
             prev_pos=current_pos;
             current_pos= {(current_pos.X_Pos+vel_dt.X_Pos+acceleration.X_Pos*dt*dt),(current_pos.Y_Pos+vel_dt.Y_Pos+acceleration.Y_Pos*dt*dt)};
@@ -46,8 +47,8 @@ public:
 
     void container_pot()
     {
-        struct VEC2 pot_pos= {500,500};
-        float pot_rad=450;
+        struct VEC2 pot_pos= {450,450};
+        float pot_rad=400;
 
         struct VEC2 to_obj= {this->current_pos.X_Pos-pot_pos.X_Pos,this->current_pos.Y_Pos-pot_pos.Y_Pos};
         float dist= sqrt(to_obj.X_Pos*to_obj.X_Pos + to_obj.Y_Pos*to_obj.Y_Pos);
@@ -56,6 +57,8 @@ public:
         if(dist>pot_rad-this->rad)
         {
             this->current_pos= {this->current_pos.X_Pos+(dist-pot_rad+this->rad)*(-n_hat.X_Pos), this->current_pos.Y_Pos+(dist-pot_rad+this->rad)*(-n_hat.Y_Pos)};
+            //this->current_pos={pot_pos.X_Pos+(dist-this->rad)*n_hat.X_Pos, pot_pos.Y_Pos+(dist-this->rad)*n_hat.Y_Pos};
+            // std::cout<<this->current_pos.X_Pos<<", "<<this->current_pos.Y_Pos<<"\n";
         }
     }
 
@@ -129,13 +132,13 @@ public:
         struct VEC2 to_obj= {this->ball1->current_pos.X_Pos-this->ball2->current_pos.X_Pos,this->ball1->current_pos.Y_Pos-this->ball2->current_pos.Y_Pos};
         float dist= sqrt(to_obj.X_Pos*to_obj.X_Pos + to_obj.Y_Pos*to_obj.Y_Pos);
         struct VEC2 n_hat= {to_obj.X_Pos/dist,to_obj.Y_Pos/dist};
-        float overlap=0;
-        if(dist>link_length)
+            float overlap=0;
+            if(dist>link_length)
             overlap=this->link_length-dist;
-        else if(dist<min_length)
+            else if(dist<min_length)
             overlap=this->min_length-dist;
-        this->ball1->current_pos= {this->ball1->current_pos.X_Pos+(0.02*overlap*n_hat.X_Pos), this->ball1->current_pos.Y_Pos+(0.02*overlap*n_hat.Y_Pos)};
-        this->ball2->current_pos= {this->ball2->current_pos.X_Pos-(0.02*overlap*n_hat.X_Pos), this->ball2->current_pos.Y_Pos-(0.02*overlap*n_hat.Y_Pos)};
+            this->ball1->current_pos= {this->ball1->current_pos.X_Pos+(0.03*overlap*n_hat.X_Pos), this->ball1->current_pos.Y_Pos+(0.03*overlap*n_hat.Y_Pos)};
+            this->ball2->current_pos= {this->ball2->current_pos.X_Pos-(0.03*overlap*n_hat.X_Pos), this->ball2->current_pos.Y_Pos-(0.03*overlap*n_hat.Y_Pos)};
 
     }
 };
@@ -148,49 +151,42 @@ int main()
     //initialize window
     int num_of_balls=0;
     int gd = DETECT, gm;
-    initwindow(1000, 1000);
+    initwindow(900, 900);
     POINT cursor_pos;
     //balls for links
     class verlet_balls* v_balls[100];
-    for(int  i=0; i<33; i++)
+    for(int  i=0; i<65; i++)
     {
         float random_rad=10;//-rand()%15;
         new verlet_balls(v_balls,i,random_rad, {280+((i%8)*35),450+(i/8)*35});
         num_of_balls++;
     }
-    class LINKS* all_links[100];
+    class LINKS* all_links[200];
     int number_of_links=0;
-
-
-    for(int i=0; i<4; i++)
+    for(int i=0; i<7; i++)
     {
-        if(i<3)
+        if(i<7)
         {
             for(int j=0; j<7; j++)
             {
-                new SOFT_LINKS(v_balls[i*8+j],v_balls[((i+1)*8)+j],20,20,all_links,number_of_links);//vertical
-                number_of_links++;
-                new SOFT_LINKS(v_balls[i*8+j],v_balls[i*8+j+1],20,20,all_links,number_of_links);//horizontal
-                number_of_links++;
-                new SOFT_LINKS(v_balls[i*8+j],v_balls[(i+1)*8+j+1],20,20,all_links,number_of_links);//diagonal->'\'
-                number_of_links++;
-                new SOFT_LINKS(v_balls[i*8+j+1],v_balls[(i+1)*8+j],20,20,all_links,number_of_links);//diagonal->'/'
-                number_of_links++;
-                if(j==6)
-                {
-                    new SOFT_LINKS(v_balls[i*8+j+1],v_balls[((i+1)*8)+j+1],20,20,all_links,number_of_links);//vertical[right most]
-                    number_of_links++;
-                }
+             ///v_balls[i*8+j]->rad+v_balls[((i+1)*8)+j]->rad+
+            new SOFT_LINKS(v_balls[i*8+j],v_balls[((i+1)*8)+j],20,20,all_links,number_of_links);//vertical
+            number_of_links++;
+            new SOFT_LINKS(v_balls[i*8+j],v_balls[i*8+j+1],20,20,all_links,number_of_links);//horizontal
+            number_of_links++;
+            new SOFT_LINKS(v_balls[i*8+j],v_balls[(i+1)*8+j+1],20,20,all_links,number_of_links);//diagona->'\'
+            number_of_links++;
+            new SOFT_LINKS(v_balls[i*8+j+1],v_balls[(i+1)*8+j],20,20,all_links,number_of_links);//diagona->'/'
+            number_of_links++;
             }
         }
         else
         {
-            for(int j=0; j<7; j++)
-            {
-                new SOFT_LINKS(v_balls[i*8+j],v_balls[i*8+j+1],20,20,all_links,number_of_links);//horizontal [bottom most]
-                number_of_links++;
-            }
-
+        for(int j=0; j<7; j++)
+        {
+        new SOFT_LINKS(v_balls[i*8+j],v_balls[i*8+j+1],20,20,all_links,number_of_links);//horizontal
+        number_of_links++;
+        }
         }
     }
 
@@ -203,6 +199,14 @@ int main()
         num_of_balls++;
     }
     v_balls[num_of_balls-1]->is_static=true;
+    v_balls[0]->is_static=true;
+    v_balls[8]->is_static=true;
+    v_balls[16]->is_static=true;
+    v_balls[24]->is_static=true;
+    v_balls[32]->is_static=true;
+    v_balls[40]->is_static=true;
+    v_balls[48]->is_static=true;
+    v_balls[52]->is_static=true;
     v_balls[num_of_balls-1]->rad=50;
 
 
@@ -217,23 +221,31 @@ int main()
         deltaTime=(clock()-oldTime)/1000.0;
         if(deltaTime>0.02)
             deltaTime=0.02;
-        circle(500,500,450);
+        circle(450,450,400);
         GetCursorPos(&cursor_pos);
         v_balls[num_of_balls-1]->current_pos= {cursor_pos.x,cursor_pos.y};
         for(int i=0; i<num_of_balls; i++)
         {
             v_balls[i]->update_pos(deltaTime,v_balls,num_of_balls);
-
-            circle(v_balls[i]->current_pos.X_Pos,v_balls[i]->current_pos.Y_Pos,1);
-            circle(v_balls[i]->current_pos.X_Pos,v_balls[i]->current_pos.Y_Pos,2);
-            putpixel(v_balls[i]->current_pos.X_Pos,v_balls[i]->current_pos.Y_Pos,RED);
+            v_balls[0]->current_pos={280,450};
+            v_balls[8]->current_pos={280,475};
+            v_balls[16]->current_pos={280,500};
+            v_balls[24]->current_pos={280,525};
+            v_balls[32]->current_pos={280,550};
+            v_balls[40]->current_pos={280,575};
+            v_balls[48]->current_pos={280,600};
+            v_balls[52]->current_pos={280,625};
             circle(v_balls[i]->current_pos.X_Pos,v_balls[i]->current_pos.Y_Pos,v_balls[i]->rad);
         }
         for(int j=0; j<number_of_links; j++)
         {
+            if(all_links[j]->ball1!=nullptr || all_links[j]->ball2!=nullptr)
+            {
             all_links[j]->update();
             line(all_links[j]->ball1->current_pos.X_Pos,all_links[j]->ball1->current_pos.Y_Pos,all_links[j]->ball2->current_pos.X_Pos,all_links[j]->ball2->current_pos.Y_Pos);
+            }
         }
+
     }
     getch();
     closegraph();
